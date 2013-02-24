46f1cc7225f8a39891f34d3512aa1c5890ae8e48
moznion <moznion@gmail.com>
2013-02-25 01:26:08 +0900

    Write naked perl module file.

--- lib/App/WithSound.pm
+++ lib/App/WithSound.pm
@@ -2,21 +2,9 @@ package App::WithSound;
 
 use warnings;
 use strict;
-use Carp;
+our $VERSION = '1.0.0';
 
-use version; $VERSION = qv('0.0.3');
-
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
-
-
-# Module implementation here
-
-
-1; # Magic true value required at end of module
+1;
 __END__
 
 =head1 NAME
@@ -37,8 +25,8 @@ This document describes App::WithSound version 0.0.1
     Brief code example(s) here showing commonest usage(s).
     This section will be as far as many users bother reading
     so make it as educational and exeplary as possible.
-  
-  
+
+
 =head1 DESCRIPTION
 
 =for author to fill in:
@@ -46,7 +34,7 @@ This document describes App::WithSound version 0.0.1
     Use subsections (=head2, =head3) as appropriate.
 
 
-=head1 INTERFACE 
+=head1 INTERFACE
 
 =for author to fill in:
     Write a separate section listing the public components of the modules
@@ -86,7 +74,7 @@ This document describes App::WithSound version 0.0.1
     files, and the meaning of any environment variables or properties
     that can be set. These descriptions must also include details of any
     configuration language used.
-  
+
 App::WithSound requires no configuration files or environment variables.
 
 
