7586347405b4b74ae813f587765d00840294b607
moznion <moznion@gmail.com>
2013-03-06 00:50:46 +0900

    Checking in changes prior to tagging of version 1.1.0.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index a5e9397..8c1ccd4 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for App-WithSound
    
    +1.1.0  Wed Mar 06 00:45:10 JST 2013
    +        - Add a new function that playbacks sound during executing the command.
    +
     1.0.2  Tue Mar 05 02:03:55 JST 2013
             - Corresponded plural audio playing application.

--- lib/App/WithSound.pm
+++ lib/App/WithSound.pm
@@ -2,7 +2,7 @@ package App::WithSound;
 
 use warnings;
 use strict;
-our $VERSION = '1.0.2';
+our $VERSION = '1.1.0';
 
 use Carp;
 use Config::Simple;
@@ -185,7 +185,7 @@ App::WithSound - Execute commands with sound
 
 =head1 VERSION
 
-This document describes App::WithSound version 1.0.2
+This document describes App::WithSound version 1.1.0
 
 
 =head1 DESCRIPTION
